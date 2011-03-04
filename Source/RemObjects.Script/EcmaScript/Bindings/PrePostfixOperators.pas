7030c08bbbfca7e0159a541c1b27e56edf6b5431
Carlo Kok <ck@remobjects.com>
2011-03-04 14:45:30 +0100

    0: Post decremental operator implemented..

--- Source/RemObjects.Script/EcmaScript/Bindings/PrePostfixOperators.pas
+++ Source/RemObjects.Script/EcmaScript/Bindings/PrePostfixOperators.pas
@@ -11,10 +11,10 @@ type
   Operators = public partial class
   public
     
-    class method PostDecrement(aLeft: Object): Object;
-    class method PostIncrement(aLeft: Object): Object;
-    class method PreDecrement(aLeft: Object): Object;
-    class method PreIncrement(aLeft: Object): Object;
+    class method PostDecrement(aLeft: Object; aExec: ExecutionContext): Object;
+    class method PostIncrement(aLeft: Object; aExec: ExecutionContext): Object;
+    class method PreDecrement(aLeft: Object; aExec: ExecutionContext): Object;
+    class method PreIncrement(aLeft: Object; aExec: ExecutionContext): Object;
 
     // OR, AND, ?: have side effects in evaluation and are not specified here.
     class var Method_PostDecrement: System.Reflection.MethodInfo := typeof(Operators).GetMethod('PostDecrement');
@@ -24,20 +24,33 @@ type
   end;
 
 implementation
-class method Operators.PostDecrement(aLeft: Object): Object;
+class method Operators.PostDecrement(aLeft: Object; aExec: ExecutionContext): Object;
 begin
-
+  var lRef := Reference(aLeft);
+  
+  if (lRef <> nil) then begin
+    if (lRef.Strict) and (lRef.Base is EnvironmentRecord) and (lRef.Name in ['eval', 'arguments']) then 
+      aExec.Global.RaiseNativeError(NativeErrorType.SyntaxError, 'eval/arguments cannot be used in post decrement operator');
+    aLeft := Reference.GetValue(lRef, aExec);
+  end;
+  var lOldValue := aLeft;
+  if aLeft is Integer then 
+    aLeft := Integer(aLeft) -1
+  else
+    aLeft := Utilities.GetObjAsDouble(aLeft) -1;
+  Reference.SetValue(lRef, aLeft, aExec);
+  exit lOldValue;
 end;
 
-class method Operators.PostIncrement(aLeft: Object): Object;
+class method Operators.PostIncrement(aLeft: Object; aExec: ExecutionContext): Object;
 begin
 end;
 
-class method Operators.PreDecrement(aLeft: Object): Object;
+class method Operators.PreDecrement(aLeft: Object; aExec: ExecutionContext): Object;
 begin
 end;
 
-class method Operators.PreIncrement(aLeft: Object): Object;
+class method Operators.PreIncrement(aLeft: Object; aExec: ExecutionContext): Object;
 begin
 end;
 
