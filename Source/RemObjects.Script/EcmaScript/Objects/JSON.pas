bbf9b2208607003d2a989a5458ee67940b19d2f1
Anton Kasyanov <antonk@remobjects.com>
2011-07-26 09:36:07 +0300

    0: JSON.pas - case mismatch fixes

--- Source/RemObjects.Script/EcmaScript/Objects/JSON.pas
+++ Source/RemObjects.Script/EcmaScript/Objects/JSON.pas
@@ -20,14 +20,15 @@ type
   GlobalObject = public partial class(EcmaScriptObject)
   public
     method CreateJSON: EcmaScriptObject;
-    method JSONParse(aCaller: ExecutionContext;aSelf: Object; params args: Array of Object): Object;
-    method JSONStringify(aCaller: ExecutionContext;aSelf: Object; params args: Array of Object): Object;
-
+    method JSONParse(aCaller: ExecutionContext;  aSelf: Object;  params args: array of Object): Object;
+    method JSONStringify(aCaller: ExecutionContext;  aSelf: Object;  params args: array of Object): Object;
     method JSONParse(aTok: Tokenizer): Object;
-    method Walk(aCaller: ExecutionContext; aRoot: EcmaScriptObject; aReviver: EcmaScriptBaseFunctionObject; aName: string; aCurrent: EcmaScriptObject): Object;
+    method Walk(aCaller: ExecutionContext;  aRoot: EcmaScriptObject;  aReviver: EcmaScriptBaseFunctionObject;
+                   aName: String;  aCurrent: EcmaScriptObject): Object;
   private
-    method JSONStr(aExecutionContext: ExecutionContext; aStack: List<Object>; aGap, aIndent: string; aReplacerFunction: EcmaScriptBaseFunctionObject; 
-      aProplist: List<String>; aWork: EcmaScriptObject; aValue: String): string;
+    method JSONStr(aExecutionContext: ExecutionContext;  aStack: List<Object>;  aGap: String;  aIndent: String;
+                   aReplacerFunction: EcmaScriptBaseFunctionObject;  aProplist: List<String>; aWork: EcmaScriptObject;
+                   aValue: String): String;
   end;
 
 
@@ -40,10 +41,12 @@ type
 
 implementation
 
+
 method GlobalObject.CreateJSON: EcmaScriptObject;
 begin
   result := EcmaScriptObject(Get(nil, 0, 'JSON'));
-  if result <> nil then exit;
+  if  (result <> nil)  then
+    exit;
 
   result := new EcmaScriptObject(self, ObjectPrototype, &Class := 'JSON');
   Values.Add('JSON', PropertyValue.NotEnum(Result));
@@ -53,199 +56,256 @@ begin
   result.Prototype := ObjectPrototype;
 end;
 
-method GlobalObject.JSONParse(aCaller: ExecutionContext;aSelf: Object; params args: Array of Object): Object;
+
+method GlobalObject.JSONParse(aCaller: ExecutionContext;  aSelf: Object;  params args: array of Object): Object;
 begin
   var lTok := new Tokenizer();
   lTok.JSON := true;
-  lTok.Error += method (Caller: Tokenizer; Kind: TokenizerErrorKind; Parameter: String); 
-    begin
-      RaiseNativeError(NativeErrorType.SyntaxError, 'Error in json data at '+caller.Row+':'+caller.Col);
-    end;
-  lTok.SetData(UTilities.GetArgAsString(args, 0, aCaller), '');
+  lTok.Error +=    method(caller: Tokenizer;  kind: TokenizerErrorKind;  parameter: String); 
+                   begin
+                     RaiseNativeError(NativeErrorType.SyntaxError, 'Error in json data at '+caller.Row+':'+caller.Col);
+                   end;
+
+  lTok.SetData(Utilities.GetArgAsString(args, 0, aCaller), '');
   result := JSONParse(lTok);
-  if lTok.Token <> TokenKind.EOF then 
+
+  if  (lTok.Token <> TokenKind.EOF)  then 
     RaiseNativeError(NativeErrorType.SyntaxError, 'EOF expected '+lTok.Row+':'+lTok.Col);
+
   var lReviver := EcmaScriptBaseFunctionObject(Utilities.GetArg(args, 1));
-  if lReviver <> nil then begin
+  if  (lReviver <> nil)  then  begin
     var lWork := new EcmaScriptObject(self);
     lWork.DefineOwnProperty('', new PropertyValue(PropertyAttributes.None, result), false);
     result := Walk(aCaller, lWork, lReviver, '', lWork);
   end;
 end;
 
+
 method GlobalObject.JSONParse(aTok: Tokenizer): Object;
 begin
-  case aTok.Token of 
-    TokenKind.K_null: begin result := nil; aTok.Next; exit; end;
-    TokenKind.K_true: begin result := true; aTok.Next; exit; end;
-    TokenKind.K_false: begin result := false; aTok.Next; exit; end;
-    TokenKind.OpeningBracket: begin
-      aTok.Next;
-      result := new EcmaScriptArrayObject(self, 0);
-      if aTok.Token = TokenKind.ClosingBracket then begin
-        aTok.Next;
-        exit;
-      end;
+  case  aTok.Token  of 
+    TokenKind.K_null:
+                   begin
+                     result := nil;
+                     aTok.Next();
+                     exit;
+                   end;
 
-      loop begin
-        EcmaScriptArrayObject(result).AddValue(JSONParse(aTok));
-        if aTok.Token = TokenKind.ClosingBracket then begin
-          aTok.Next;
-          exit;
-        end;
-        if atok.Token <> TokenKind.Comma then begin
-          RaiseNativeError(NativeErrorType.SyntaxError, 'Comma expected at '+aTok.Row+':'+aTok.Col);
-        end;
-        aTok.Next;
-      end;
-    end;
+    TokenKind.K_true:
+                   begin
+                     result := true;
+                     aTok.Next();
+                     exit;
+                   end;
 
-    TokenKind.CurlyOpen: begin
-      aTok.Next;
-      result := new EcmaScriptObject(self);
-      if aTok.Token = TokenKind.CurlyClose then begin
-        aTok.Next;
-        exit;
-      end;
-      loop begin
-        if aTok.Token <> TokenKind.String then 
-          RaiseNativeError(NativeErrorType.SyntaxError, 'String expected at '+aTok.Row+':'+aTok.Col);
-        var lKey := Tokenizer.DecodeString(aTok.TokenStr);
-        aTok.Next;
-        if aTok.Token <> TokenKind.Colon then 
-          RaiseNativeError(NativeErrorType.SyntaxError, 'Colon expected at '+aTok.Row+':'+aTok.Col);
-        aTok.Next;
-        var lValue := JSONParse(aTok);
-        EcmaScriptObject(result).AddValue(lKey, lValue);
-        if aTok.Token = TokenKind.CurlyClose then begin
-          aTok.Next;
-          exit;
-        end;
-        if atok.Token <> TokenKind.Comma then begin
-          RaiseNativeError(NativeErrorType.SyntaxError, 'Comma expected at '+aTok.Row+':'+aTok.Col);
-        end;
-        aTok.Next;
-      end;
-    end;
+    TokenKind.K_false:
+                   begin
+                     result := false;
+                     aTok.Next();
+                     exit;
+                   end;
 
-    TokenKind.String: begin
-      result := Tokenizer.DecodeString(aTok.TokenStr);
-      aTok.Next;
-      exit;
-    end;
-    TokenKind.Float: begin
-      var d: Double;
-      if not Double.TryParse(aTok.TokenStr, System.Globalization.NumberStyles.Float, System.Globalization.NumberFormatInfo.InvariantInfo, out d) then
-        RaiseNativeError(NativeErrorType.SyntaxError, 'Number expected at '+aTok.Row+':'+aTok.Col);
-      aTok.Next;
-      exit d;
-    end;
-    TokenKind.Number: begin
-      var i: Integer; 
-      var i6: Int64;
-      if not Int32.TryParse(aTok.TokenStr, out i) then begin
-        if Int64.TryParse(aTok.Tokenstr, out i6) then exit i6;
-        RaiseNativeError(NativeErrorType.SyntaxError, 'Number expected at '+aTok.Row+':'+aTok.Col);
-      end;
-      aTok.Next;
-      exit i;
-    end;
-  else
-    RaiseNativeError(NativeERrorType.SyntaxError, 'JSON object expected at '+aTok.Row+':'+aTok.Col);
-  end; // case
+    TokenKind.OpeningBracket:
+                   begin
+                     aTok.Next;
+                     result := new EcmaScriptArrayObject(self, 0);
+                     if  (aTok.Token = TokenKind.ClosingBracket)  then  begin
+                       aTok.Next();
+                       exit;
+                     end;
+
+                     loop  begin
+                       EcmaScriptArrayObject(result).AddValue(JSONParse(aTok));
+                       if  (aTok.Token = TokenKind.ClosingBracket)  then  begin
+                         aTok.Next();
+                         exit;
+                       end;
+
+                       if  (aTok.Token <> TokenKind.Comma)  then
+                         RaiseNativeError(NativeErrorType.SyntaxError, 'Comma expected at '+aTok.Row+':'+aTok.Col);
+
+                       aTok.Next();
+                     end;
+                   end;
+
+    TokenKind.CurlyOpen:
+                   begin
+                     aTok.Next;
+                     result := new EcmaScriptObject(self);
+                     if  (aTok.Token = TokenKind.CurlyClose)  then  begin
+                       aTok.Next();
+                       exit;
+                     end;
+
+                     loop  begin
+                       if  (aTok.Token <> TokenKind.String)  then
+                         RaiseNativeError(NativeErrorType.SyntaxError, 'String expected at '+aTok.Row+':'+aTok.Col);
+
+                       var lKey := Tokenizer.DecodeString(aTok.TokenStr);
+                       aTok.Next();
+
+                       if  (aTok.Token <> TokenKind.Colon)  then
+                         RaiseNativeError(NativeErrorType.SyntaxError, 'Colon expected at '+aTok.Row+':'+aTok.Col);
+                       aTok.Next();
+
+                       var lValue := JSONParse(aTok);
+                       EcmaScriptObject(result).AddValue(lKey, lValue);
+                       if  (aTok.Token = TokenKind.CurlyClose)  then  begin
+                         aTok.Next();
+                         exit;
+                       end;
+
+                       if  (aTok.Token <> TokenKind.Comma)  then
+                         RaiseNativeError(NativeErrorType.SyntaxError, 'Comma expected at '+aTok.Row+':'+aTok.Col);
+
+                       aTok.Next();
+                     end;
+                   end;
+
+    TokenKind.String:
+                   begin
+                     result := Tokenizer.DecodeString(aTok.TokenStr);
+                     aTok.Next();
+                     exit;
+                   end;
+
+    TokenKind.Float:
+                   begin
+                     var d: Double;
+                     if  (not Double.TryParse(aTok.TokenStr, System.Globalization.NumberStyles.Float, System.Globalization.NumberFormatInfo.InvariantInfo, out d))  then
+                       RaiseNativeError(NativeErrorType.SyntaxError, 'Number expected at '+aTok.Row+':'+aTok.Col);
+
+                     aTok.Next();
+                     exit (d);
+                   end;
+
+    TokenKind.Number:
+                   begin
+                     var i: Integer; 
+                     if  (not Int32.TryParse(aTok.TokenStr, out i))  then  begin
+                       var i6: Int64;
+                       if  (Int64.TryParse(aTok.TokenStr, out i6))  then
+                         exit  (i6);
+
+                       RaiseNativeError(NativeErrorType.SyntaxError, 'Number expected at '+aTok.Row+':'+aTok.Col);
+                     end;
+
+                     aTok.Next;
+                     exit  (i);
+                   end;
+
+    else           RaiseNativeError(NativeErrorType.SyntaxError, 'JSON object expected at '+aTok.Row+':'+aTok.Col);
+  end;
 end;
 
-method GlobalObject.JSONStringify(aCaller: ExecutionContext;aSelf: Object; params args: Array of Object): Object;
+
+method GlobalObject.JSONStringify(aCaller: ExecutionContext;  aSelf: Object;  params args: array of Object): Object;
 begin
-  var lStack := new List<Object>;
-  var lIndent := string.Empty;
-  var lGap: string := String.Empty;
+  var lStack: List<Object> := new List<Object>();
+  var lIndent: String := String.Empty;
+  var lGap: String := String.Empty;
   var lReplacerFunction := EcmaScriptBaseFunctionObject(Utilities.GetArg(args, 1));
-  var lProplist: List<string> := nil;
-  if lReplacerFunction = nil then begin 
-    var lItem :=  EcmaScriptArrayObject(Utilities.GetArg(Args, 1));
-    lPropList := new List<String>;
-    if lItem <> nil then begin
-      for i: Integer := 0 to lItem.Length -1 do begin
-        var lEl := lItem.Get(aCaller, 0, i.ToString);
-        if (lEl = nil) or (lEl =  Undefined.Instance) then continue;
-        lPropList.Add(lEl.ToString);
+  var lPropList: List<String> := nil;
+
+  if  (lReplacerFunction = nil)  then  begin
+    var lItem :=  EcmaScriptArrayObject(Utilities.GetArg(args, 1));
+    lPropList := new List<String>();
+    if  (lItem <> nil)  then  begin
+      for  i: Int32  :=  0  to  lItem.Length-1  do  begin
+        var lEl := lItem.Get(aCaller, 0, i.ToString());
+        if  ((lEl = nil)  or  (lEl =  Undefined.Instance))  then
+          continue;
+
+        lPropList.Add(lEl.ToString());
       end;
     end;
   end;
+
   var lSpace := Utilities.GetArg(args, 2);
-  if lSpace is EcmaScriptObject then begin
-    if EcmaScriptObject(lSpace).Class = 'Number' then
+  if  (lSpace is EcmaScriptObject)  then  begin
+    if  (EcmaScriptObject(lSpace).Class = 'Number')  then
       lGap := new String(' ', Utilities.GetObjAsInteger(lSpace, aCaller))
-    else if EcmaScriptObject(lSpace).Class = 'String' then
+    else if  (EcmaScriptObject(lSpace).Class = 'String')  then
       lGap := Utilities.GetObjAsString(lSpace, aCaller);
-  end else if lSpace is string then
+  end
+  else if  (lSpace is String)  then
     lGap := String(lSpace)
-  else if (lSpace is Int32) or (lSpace is Double) then
+  else if  ((lSpace is Int32) or (lSpace is Double))  then
     lGap := new String(' ', Utilities.GetObjAsInteger(lSpace, aCaller));
-  if Length(lGap) > 10 then lGap := lGap.Substring(0,10);
+
+  if  (length(lGap) > 10)  then
+    lGap := lGap.Substring(0,10);
+
   var lWork := new EcmaScriptObject(self);
   lWork.DefineOwnProperty('', new PropertyValue(PropertyAttributes.All, Utilities.GetArg(args, 0)), false);
-  exit JSONStr(aCaller, lStack, lGap, lIndent, lReplacerFunction, lProplist, lWork, '');
+
+  exit  (self.JSONStr(aCaller, lStack, lGap, lIndent, lReplacerFunction, lPropList, lWork, ''));
 end;
 
 
-method GlobalObject.Walk(aCaller: ExecutionContext; 
-  aRoot: EcmaScriptObject; 
-  aReviver: EcmaScriptBaseFunctionObject; 
-  aName: string; aCurrent: EcmaScriptObject): Object;
+method GlobalObject.Walk(aCaller: ExecutionContext;  aRoot: EcmaScriptObject;  aReviver: EcmaScriptBaseFunctionObject;
+                   aName: String;  aCurrent: EcmaScriptObject): Object;
 begin
   var lItem := aCurrent.Get(aCaller, 2, aName);
   var lEc := EcmaScriptObject(lItem);
-  if lEc <> nil then begin
+  if  (lEc <> nil)  then  begin
     var lArr := EcmaScriptArrayObject(lEc);
-    if lArr <> nil then begin
-      var i: Integer := 0;
-      var lLen := Integer(lArr.Length);
-      while i < lLen do begin
-        var lNewVal := Walk(aCaller, aRoot, aReviver,i.ToString, lArr);
-        if lNewVal = Undefined.Instance then
-          lArr.Delete(i.ToString(), false)
-        else begin
-          lArr.Put(acaller, i.ToString(), lNewVal);
+    if  (lArr <> nil)  then  begin
+      var i: Int32 := 0;
+      var lLen: Int32 := Int32(lArr.Length);
+      while  (i < lLen)  do  begin
+        var lNewVal := self.Walk(aCaller, aRoot, aReviver, i.ToString(), lArr);
+        if  (lNewVal = Undefined.Instance)  then  begin
+          lArr.Delete(i.ToString(), false);
+        end
+        else  begin
+          lArr.Put(aCaller, i.ToString(), lNewVal);
           inc(i);
         end;
       end;
-    end else begin
-      for each el in System.Linq.Enumerable.ToArray(lEc.Values.Keys) do begin // copy it first
+    end
+    else  begin
+      for each  el  in  System.Linq.Enumerable.ToArray(lEc.Values.Keys)  do  begin // copy it first
         var lNewVal := Walk(aCaller, aRoot, aReviver, el, lArr);
-        if lNewVal = undefined.Instance then
+        if  (lNewVal = Undefined.Instance)  then
           lEc.Delete(el, false)
         else
           lEc.DefineOwnProperty(el, new PropertyValue(PropertyAttributes.None, lNewVal), false);
       end;
     end;
   end;
-  exit aReviver.CallEx(aCaller, aRoot,aName, lItem);
+
+  exit  (aReviver.CallEx(aCaller, aRoot,aName, lItem));
 end;
 
 
-method GlobalObject.JSONStr(aExecutionContext: ExecutionContext; aStack: List<Object>; aGap, aIndent: string; aReplacerFunction: EcmaScriptBaseFunctionObject; 
-      aProplist: List<String>; aWork: EcmaScriptObject; aValue: String): string;
+method GlobalObject.JSONStr(aExecutionContext: ExecutionContext;  aStack: List<Object>;  aGap: String;  aIndent: String;
+                   aReplacerFunction: EcmaScriptBaseFunctionObject;  aProplist: List<String>;  aWork: EcmaScriptObject;
+                   aValue: String): String;
 begin
-  var value := aWork.Get(aExecutionContext, 0, aValue);
+  var Value := aWork.Get(aExecutionContext, 0, aValue);
+
   var lObj := EcmaScriptObject(Value);
   if  (assigned(lObj))  then  begin
     var lCall := EcmaScriptBaseFunctionObject(lObj.Get(aExecutionContext, 0, 'toJSON'));
     if  (assigned(lCall))  then
       exit  (String(lCall.CallEx(aExecutionContext, lObj, aValue)));
   end;
-  if aReplacerFunction <> nil then begin
-    value := aReplacerFunction.CallEx(aExecutionContext, aWork,aValue, Value);
-  end;
-  lObj := EcmaScriptObject(VAlue);
-  if lObj <> nil then begin
-    if lobj.Class = 'Number' then Value := lObj.Value else
-    if lObj.Class = 'String' then Value := lObj.Value;
-    if lObj.Class = 'Boolean' then Value := lObj.Value;
+
+  if  (aReplacerFunction <> nil)  then
+    Value := aReplacerFunction.CallEx(aExecutionContext, aWork,aValue, Value);
+
+  lObj := EcmaScriptObject(Value);
+  if  (lObj <> nil)  then  begin
+    if  (lObj.Class = 'Number')  then
+      Value := lObj.Value
+    else if  (lObj.Class = 'String')  then
+      Value := lObj.Value
+    else if  (lObj.Class = 'Boolean')  then
+      Value := lObj.Value;
   end;
 
-  var lResult: String := JSON.ToString(value);
+  var lResult: String := JSON.ToString(Value);
   if  (assigned(lResult))  then
     exit  (lResult);
 
@@ -296,17 +356,17 @@ begin
         end;
       end;
     end; 
-  end else begin
-    var lStepBack := aIndent;
+  end
+  else begin
     aIndent := aIndent + aGap;
     var k := aProplist:ToArray;
-    if Length(k) = 0 then begin
+    if  (length(k) = 0)  then  begin
       if lObj is EcmaScriptObjectWrapper then
         k := EcmaScriptObjectWrapper(lObj).GetOwnNames.ToArray
       else
         k := lObj.Values.Where(a->PropertyAttributes.Enumerable in a.Value.Attributes).Select(a->a.Key).ToArray;
     end;
-    var lItems := new List<string>;
+    var lItems: List<String> := new List<String>();
     for each el in k do begin
       var lVal := JSONStr(aExecutionContext, aStack, aGap, aIndent, aReplacerFunction, aProplist, lObj, el);
       if lVal <> nil then begin
