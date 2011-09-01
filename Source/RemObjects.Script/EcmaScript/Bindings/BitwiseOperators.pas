268b2b46ece110da6322c3ff1d0c76d0bf6bce60
Anton Kasyanov <antonk@remobjects.com>
2011-09-01 15:42:26 +0300

    0: BitwiseOperators.pas - case mismatch fixes

--- Source/RemObjects.Script/EcmaScript/Bindings/BitwiseOperators.pas
+++ Source/RemObjects.Script/EcmaScript/Bindings/BitwiseOperators.pas
@@ -1,42 +1,45 @@
-
 namespace RemObjects.Script.EcmaScript;
 
 interface
 
 uses
   System.Collections.Generic,
-  System.Text,
   System.Runtime.CompilerServices,
+  System.Text,
   RemObjects.Script;
+
 type
   Operators = public partial class
-  private
-  protected
   public
     class method &And(aLeft, aRight: Object; ec: ExecutionContext): Object;
     class method &Or(aLeft, aRight: Object; ec: ExecutionContext): Object;
     class method &Xor(aLeft, aRight: Object; ec: ExecutionContext): Object;
 
-    class var Method_And: System.Reflection.MethodInfo := typeof(Operators).GetMethod('And');
-    class var Method_Or: System.Reflection.MethodInfo := typeof(Operators).GetMethod('Or');
-    class var Method_XOr: System.Reflection.MethodInfo := typeof(Operators).GetMethod('Xor');
+    class var Method_And: System.Reflection.MethodInfo := typeOf(Operators).GetMethod('And');
+    class var Method_Or: System.Reflection.MethodInfo := typeOf(Operators).GetMethod('Or');
+    class var Method_XOr: System.Reflection.MethodInfo := typeOf(Operators).GetMethod('Xor');
   end;
+
 
 implementation
 
+
 class method Operators.And(aLeft, aRight: Object; ec: ExecutionContext): Object;
 begin
-  exit Utilities.GetObjAsInteger(aLeft, ec) and Utilities.GetObjAsInteger(aRight, ec);
+  exit  (Utilities.GetObjAsInteger(aLeft, ec) and Utilities.GetObjAsInteger(aRight, ec));
 end;
+
 
 class method Operators.Or(aLeft, aRight: Object; ec: ExecutionContext): Object;
 begin
-  exit Utilities.GetObjAsInteger(aLeft, ec) or Utilities.GetObjAsInteger(aRight, ec);
+  exit  (Utilities.GetObjAsInteger(aLeft, ec) or Utilities.GetObjAsInteger(aRight, ec));
 end;
+
 
 class method Operators.Xor(aLeft, aRight: Object; ec: ExecutionContext): Object;
 begin
-  exit Utilities.GetObjAsInteger(aLeft, ec) xor Utilities.GetObjAsInteger(aRight, ec);
+  exit  (Utilities.GetObjAsInteger(aLeft, ec) xor Utilities.GetObjAsInteger(aRight, ec));
 end;
+
 
 end.
