ede6de69ab4a240e3fe3aebc1bc3ce0006b5bc6c
Anton Kasyanov <antonk@remobjects.com>
2012-06-06 11:38:56 +0300

    56145: Set proper attributes in AssemblyInfo for all Script/.NET samples

--- Samples/C#/Debugger/MainForm.cs
+++ Samples/C#/Debugger/MainForm.cs
@@ -1,57 +1,77 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 
 namespace Debugger
 {
-	public partial class MainForm : Form
-	{
-        public delegate object MyDelegate(params object[] args);
-		public MainForm ()
-		{
-			InitializeComponent ();
-			tbMain.Document.HighlightingStrategy = ICSharpCode.TextEditor.Document.HighlightingManager.Manager.FindHighlighterForFile("file.js");
-			tbMain.TextChanged += new EventHandler (tbMain_TextChanged);
+    public partial class MainForm : Form
+    {
+        public delegate Object MyDelegate(params Object[] args);
+
+        public MainForm()
+        {
+            InitializeComponent();
+            tbMain.Document.HighlightingStrategy = ICSharpCode.TextEditor.Document.HighlightingManager.Manager.FindHighlighterForFile("file.js");
+            tbMain.TextChanged += new EventHandler(tbMain_TextChanged);
             ScriptEngine.RunInThread = true;
-			ScriptEngine.Debug = true;
+            ScriptEngine.Debug = true;
             ScriptEngine.StatusChanged += new EventHandler(ScriptEngine_StatusChanged);
             ScriptEngine.DebugTracePoint += new EventHandler<RemObjects.Script.ScriptDebugEventArgs>(ScriptEngine_DebugTracePoint);
             ScriptEngine.DebugThreadExit += new EventHandler<RemObjects.Script.ScriptDebugEventArgs>(ScriptEngine_DebugThreadExit);
-            ScriptEngine.Globals.SetVariable("writeln", new MyDelegate(delegate (object[] args) {
-                Invoke(new MyDelegate(delegate(object[] args2) {
-                    string s = "";
-                    if (args2 != null) {
-                        s = String.Join(" ",(from x in args2 select x == null?"":x.ToString()).ToArray());
-                    }
-                    s+= "\r\n";
+            ScriptEngine.Globals.SetVariable("writeln", new MyDelegate(delegate(Object[] args)
+            {
+                Invoke(new MyDelegate(delegate(Object[] args2)
+                {
+                    String s = "";
+                    if (args2 != null)
+                        s = String.Join(" ", (from x in args2 select x == null ? "" : x.ToString()).ToArray());
+
+                    s += "\r\n";
                     edOutput.AppendText(s);
-					this.tabs.SelectedTab = tbOutput;
+                    this.tabs.SelectedTab = tbOutput;
                     return null;
-                }),  new object[] {args});
+                }), new Object[] { args });
                 return null;
             }));
-		}
+        }
 
-        void ScriptEngine_StatusChanged(object sender, EventArgs e)
+        void ScriptEngine_StatusChanged(Object sender, EventArgs e)
         {
             RemObjects.Script.ScriptStatus lStatus = ScriptEngine.Status;
             BeginInvoke(new Action(delegate
             {
-                switch (lStatus) {
-                    case RemObjects.Script.ScriptStatus.Paused: Text = "Script Editor [Paused]"; break;
-                    case RemObjects.Script.ScriptStatus.Pausing: Text = "Script Editor [Pausing]"; break;
+                switch (lStatus)
+                {
+                    case RemObjects.Script.ScriptStatus.Paused:
+                        Text = "Script Editor [Paused]";
+                        break;
+
+                    case RemObjects.Script.ScriptStatus.Pausing:
+                        Text = "Script Editor [Pausing]";
+                        break;
+
                     case RemObjects.Script.ScriptStatus.Running:
                         edOutput.Clear();
-                        Text = "Script Editor [Running]"; break;
-                    case RemObjects.Script.ScriptStatus.StepInto: Text = "Script Editor [Running (Step Into)]"; break;
-                    case RemObjects.Script.ScriptStatus.StepOut: Text = "Script Editor [Running (Step Out)]"; break;
-                    case RemObjects.Script.ScriptStatus.StepOver: Text = "Script Editor [Running (Step Over)]"; break;
-                    case RemObjects.Script.ScriptStatus.Stopping: Text = "Script Editor [Stopping]"; break;
+                        Text = "Script Editor [Running]";
+                        break;
+
+                    case RemObjects.Script.ScriptStatus.StepInto:
+                        Text = "Script Editor [Running (Step Into)]";
+                        break;
+
+                    case RemObjects.Script.ScriptStatus.StepOut:
+                        Text = "Script Editor [Running (Step Out)]";
+                        break;
+
+                    case RemObjects.Script.ScriptStatus.StepOver:
+                        Text = "Script Editor [Running (Step Over)]";
+                        break;
+
+                    case RemObjects.Script.ScriptStatus.Stopping:
+                        Text = "Script Editor [Stopping]";
+                        break;
+
                     default:
                         if (ScriptEngine.RunException != null)
                         {
@@ -59,35 +79,35 @@ namespace Debugger
                             this.tabs.SelectedTab = tbOutput;
                         }
                         else
+                        {
                             edOutput.AppendText("\r\nResult: \r\n" + ScriptEngine.RunResult);
-           
+                        }
                         tbMain.Document.MarkerStrategy.RemoveAll(a => (a.TextMarkerType == ICSharpCode.TextEditor.Document.TextMarkerType.SolidBlock && a.Color == Color.Red));
                         tbMain.Refresh();
                         Text = "Script Editor"; // done running
-						break;
-                   
+                        break;
                 }
             }));
         }
 
-        void ScriptEngine_DebugThreadExit(object sender, RemObjects.Script.ScriptDebugEventArgs e)
+        void ScriptEngine_DebugThreadExit(Object sender, RemObjects.Script.ScriptDebugEventArgs e)
         {
             Invoke(new Action(delegate
                 {
-            tbMain.Document.MarkerStrategy.RemoveAll(a => (a.TextMarkerType == ICSharpCode.TextEditor.Document.TextMarkerType.SolidBlock && a.Color == Color.Red));
-            tbMain.Refresh();
+                    tbMain.Document.MarkerStrategy.RemoveAll(a => (a.TextMarkerType == ICSharpCode.TextEditor.Document.TextMarkerType.SolidBlock && a.Color == Color.Red));
+                    tbMain.Refresh();
                 }));
         }
 
-        void ScriptEngine_DebugTracePoint(object sender, RemObjects.Script.ScriptDebugEventArgs e)
+        void ScriptEngine_DebugTracePoint(Object sender, RemObjects.Script.ScriptDebugEventArgs e)
         {
             if (e.SourceSpan.IsValid)
             {
                 Invoke(new Action(delegate
                 {
                     tbMain.Document.MarkerStrategy.RemoveAll(a => (a.TextMarkerType == ICSharpCode.TextEditor.Document.TextMarkerType.SolidBlock && a.Color == Color.Red));
-                    int lStart = tbMain.Document.PositionToOffset(new ICSharpCode.TextEditor.TextLocation(e.SourceSpan.Start.Column - 1, e.SourceSpan.Start.Line - 1));
-                    int lEnd = tbMain.Document.PositionToOffset(new ICSharpCode.TextEditor.TextLocation(e.SourceSpan.End.Column - 1, e.SourceSpan.End.Line - 1));
+                    Int32 lStart = tbMain.Document.PositionToOffset(new ICSharpCode.TextEditor.TextLocation(e.SourceSpan.Start.Column - 1, e.SourceSpan.Start.Line - 1));
+                    Int32 lEnd = tbMain.Document.PositionToOffset(new ICSharpCode.TextEditor.TextLocation(e.SourceSpan.End.Column - 1, e.SourceSpan.End.Line - 1));
                     tbMain.Document.MarkerStrategy.AddMarker(new ICSharpCode.TextEditor.Document.TextMarker(lStart, lEnd - lStart + 1, ICSharpCode.TextEditor.Document.TextMarkerType.SolidBlock, Color.Red));
                     tbMain.Refresh();
                 }));
@@ -103,16 +123,18 @@ namespace Debugger
             Invoke(new Action(delegate
             {
                 lvLocals.Items.Clear();
-                for (int i = ScriptEngine.CallStack.Count -1; i >= 0; i--) {
+                for (Int32 i = ScriptEngine.CallStack.Count - 1; i >= 0; i--)
+                {
                     var el = ScriptEngine.CallStack[i];
                     ListViewItem item = new ListViewItem("[METHOD]");
                     item.SubItems.Add(el.Method ?? null);
                     lvLocals.Items.Add(item);
                     item = new ListViewItem("this");
-                    item.SubItems.Add(el.This == null? "this" : el.This.ToString());
+                    item.SubItems.Add(el.This == null ? "this" : el.This.ToString());
                     lvLocals.Items.Add(item);
-                    
-                    foreach (var name in el.Frame.Names()) {
+
+                    foreach (var name in el.Frame.Names())
+                    {
                         item = new ListViewItem(name);
                         var val = el.Frame.GetBindingValue(name, false);
                         item.SubItems.Add(val == null ? "(null)" : val.ToString());
@@ -122,97 +144,95 @@ namespace Debugger
             }));
         }
 
-        
+        void tbMain_TextChanged(Object sender, EventArgs e)
+        {
+            Changed();
+        }
 
-		void tbMain_TextChanged (object sender, EventArgs e)
-		{
-			Changed ();
-		}
+        private void stepIntoToolStripMenuItem_Click(Object sender, EventArgs e)
+        {
+            cmdStepInto();
+        }
 
-		private void stepIntoToolStripMenuItem_Click (object sender, EventArgs e)
-		{
-			cmdStepInto ();
-		}
+        private void stopToolStripMenuItem_Click(Object sender, EventArgs e)
+        {
+            cmdStop();
+        }
 
-		private void stopToolStripMenuItem_Click (object sender, EventArgs e)
-		{
-			cmdStop ();
-		}
+        private void setClearBreakpointToolStripMenuItem_Click(Object sender, EventArgs e)
+        {
+            cmdSetBreakpoint();
+        }
 
-		private void setClearBreakpointToolStripMenuItem_Click (object sender, EventArgs e)
-		{
-			cmdSetBreakpoint ();
-		}
+        private void stepOutToolStripMenuItem_Click(Object sender, EventArgs e)
+        {
+            cmdStepOut();
+        }
 
-		private void stepOutToolStripMenuItem_Click (object sender, EventArgs e)
-		{
-			cmdStepOut ();
-		}
+        private void stepOverToolStripMenuItem_Click(Object sender, EventArgs e)
+        {
+            cmdStepOver();
+        }
 
-		private void stepOverToolStripMenuItem_Click (object sender, EventArgs e)
-		{
-			cmdStepOver ();
-		}
+        private void runToolStripMenuItem_Click(Object sender, EventArgs e)
+        {
+            cmdRun();
+        }
 
-		private void runToolStripMenuItem_Click (object sender, EventArgs e)
-		{
-			cmdRun ();
-		}
-
-		private void debugToolStripMenuItem_Click (object sender, EventArgs e)
-		{
-			runToolStripMenuItem.Enabled = IsPaused () || !IsDebugging ();
-			stepIntoToolStripMenuItem.Enabled = runToolStripMenuItem.Enabled;
-			stepOutToolStripMenuItem.Enabled = IsPaused ();
+        private void debugToolStripMenuItem_Click(Object sender, EventArgs e)
+        {
+            runToolStripMenuItem.Enabled = IsPaused() || !IsDebugging();
+            stepIntoToolStripMenuItem.Enabled = runToolStripMenuItem.Enabled;
+            stepOutToolStripMenuItem.Enabled = IsPaused();
             stepOverToolStripMenuItem.Enabled = runToolStripMenuItem.Enabled;
-			setClearBreakpointToolStripMenuItem.Enabled = true;
-			stopToolStripMenuItem.Enabled = IsDebugging ();
-		}
+            setClearBreakpointToolStripMenuItem.Enabled = true;
+            stopToolStripMenuItem.Enabled = IsDebugging();
+        }
 
-		private void newToolStripMenuItem_Click (object sender, EventArgs e)
-		{
-			cmdNew ();
-		}
+        private void newToolStripMenuItem_Click(Object sender, EventArgs e)
+        {
+            cmdNew();
+        }
 
-		private void openToolStripMenuItem_Click (object sender, EventArgs e)
-		{
-			cmdOpen ();
-		}
+        private void openToolStripMenuItem_Click(Object sender, EventArgs e)
+        {
+            cmdOpen();
+        }
 
-		private void saveToolStripMenuItem_Click (object sender, EventArgs e)
-		{
-			cmdSave ();
-		}
+        private void saveToolStripMenuItem_Click(Object sender, EventArgs e)
+        {
+            cmdSave();
+        }
 
-		private void saveAsToolStripMenuItem_Click (object sender, EventArgs e)
-		{
-			cmdSaveAs ();
-		}
+        private void saveAsToolStripMenuItem_Click(Object sender, EventArgs e)
+        {
+            cmdSaveAs();
+        }
 
-		private void exitToolStripMenuItem_Click (object sender, EventArgs e)
-		{
-			cmdQuit ();
-		}
+        private void exitToolStripMenuItem_Click(Object sender, EventArgs e)
+        {
+            cmdQuit();
+        }
 
-		private void fileToolStripMenuItem_Click (object sender, EventArgs e)
-		{
-			newToolStripMenuItem.Enabled = CanFileCommands ();
-			openToolStripMenuItem.Enabled = CanFileCommands ();
-			saveAsToolStripMenuItem.Enabled = CanFileCommands ();
-			saveToolStripMenuItem.Enabled = CanFileCommands ();
-			exitToolStripMenuItem.Enabled = CanFileCommands ();
-		}
+        private void fileToolStripMenuItem_Click(Object sender, EventArgs e)
+        {
+            newToolStripMenuItem.Enabled = CanFileCommands();
+            openToolStripMenuItem.Enabled = CanFileCommands();
+            saveAsToolStripMenuItem.Enabled = CanFileCommands();
+            saveToolStripMenuItem.Enabled = CanFileCommands();
+            exitToolStripMenuItem.Enabled = CanFileCommands();
+        }
 
-		private void MainForm_FormClosing (object sender, FormClosingEventArgs e)
-		{
-			if(!CanQuit ())
-				e.Cancel = true;
-		}
+        private void MainForm_FormClosing(Object sender, FormClosingEventArgs e)
+        {
+            if (!CanQuit())
+                e.Cancel = true;
+        }
 
-		private void aboutToolStripMenuItem_Click (object sender, EventArgs e)
-		{
-			CmdAbout ();
-		}
+        private void aboutToolStripMenuItem_Click(Object sender, EventArgs e)
+        {
+            CmdAbout();
+        }
 
-	}
+    }
 }
--- Samples/C#/Debugger/Properties/AssemblyInfo.cs
+++ Samples/C#/Debugger/Properties/AssemblyInfo.cs
@@ -1,36 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle ("Debugger")]
-[assembly: AssemblyDescription ("")]
-[assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("RemObjects")]
-[assembly: AssemblyProduct ("Debugger")]
-[assembly: AssemblyCopyright ("Copyright © RemObjects Software 2011")]
-[assembly: AssemblyTrademark ("")]
-[assembly: AssemblyCulture ("")]
+[assembly: AssemblyTitle("Script Debugger Sample")]
+[assembly: AssemblyDescription("Script Debugger Sample")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("RemObjects Software LLC")]
+[assembly: AssemblyProduct("RemObjects Script for .NET")]
+[assembly: AssemblyCopyright("Copyright RemObjects Software LLC. 2009-2012. All Rights Reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible (false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid ("419974b1-dca4-42ca-a208-3374c7525a4c")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion ("1.0.0.0")]
-[assembly: AssemblyFileVersion ("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
